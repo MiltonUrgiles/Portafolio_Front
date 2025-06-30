7dfee4aef30d51bb5de6c70859b74ef100e80a5a
MiltonUrgiles <Jolu20x@gmail.com>
2025-06-29 22:11:10 -0500

    F2

--- src/components/config.js
+++ src/components/config.js
@@ -1,5 +1,5 @@
 // config.js
-export const BASE_URL = 'http://www.micarpeta.somee.com/api/pdf';
+export const BASE_URL = 'https://www.micarpeta.somee.com/api/pdf';
 
 //export const BASE_URL = 'http://localhost:5000';
 
